232ee1ca11e3812f2421b3153f99390a5971d452
amadodiazjr <amado.diaz.jr@gmail.com>
2025-06-21 09:34:36 -0400

    prism added but not quite ready yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
+import { useRef } from 'react';
+//import 'prismjs/components/prism-typescript'
+// import 'prismjs/components/prism-bash'
+// import 'prismjs/plugins/line-numbers/prism-line-numbers'
+
 import './App.css'
 
+const test = `
+describe('sub page', () => { 
+  it('shall return response.status 200', async () => {
+    // given
+    const url = '${window.location.href}'
+    
+    // when
+    const response = await fetch(url)
+
+    // then
+    expect(response.status).toBe(200)
+  })
+})`.trim()
+
 function App() {
+  const codeTestRef: any = useRef<HTMLElement>(null)
+
   return (
     <>
-      <h1 className="text-4xl underline">
-        Hello world!
-      </h1>
+      <div className="flex">
+        <div className="w-64 flex-1 bg-pink-500 m-4 rounded-md overflow-x-auto ...">
+          <div>
+            <div>
+              <pre className="line-numbers" data-line="10" >
+                <code ref={codeTestRef} className="language-typescript">
+                  {test}
+                </code>
+              </pre>
+            </div>
+          </div>
+        </div>
+      </div>    
     </>
   )
 }
